b3454e33a1632bce18c32c5b93331ee5ab1a7d8f
Juan-Chen-BNUZ <BNUZ_Juan@163.com>
2020-05-11 11:26:14 +0800

    update

---
+++ Test/test-26.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+"""
+题目描述
+输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向。
+"""
+
+
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def Convert(self, root):
+        if not root:
+            return None
+        if not root.left and not root.right:
+            return root
+
+        # 将左子树构建成双链表，返回链表头
+        left = self.Convert(root.left)
+        p = left
+
+        # 定位至左子树的最右的一个结点
+        while left and p.right:
+            p = p.right
+
+        # 如果左子树不为空，将当前root加到左子树链表
+        if left:
+            p.right = root
+            root.left = p
+
+        # 将右子树构造成双链表，返回链表头
+        right = self.Convert(root.right)
+        # 如果右子树不为空，将该链表追加到root结点之后
+        if right:
+            right.left = root
+            root.right = right
+
+        return left if left else root
